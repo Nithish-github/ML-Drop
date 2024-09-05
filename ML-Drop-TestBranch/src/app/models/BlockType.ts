fd2f72aea0551cef11e455e844bc76a2f720fff2
Nithish-github <hiiamnithish@gmail.com>
2024-09-05 12:21:19 +0530

    sep 5

--- ML-Drop-TestBranch/src/app/models/BlockType.ts
+++
@@ -1,4 +0,0 @@
-export enum BlockType {
-  Image_Block,
-  ML_Block,
-}
--- ML-Drop-TestBranch/src/app/models/block.ts
+++
@@ -1,9 +0,0 @@
-import { BlockType } from './BlockType';
-
-export class Block {
-  public id!: number;
-  public name!: string;
-  public type!: BlockType;
-  public position = { x: 0, y: 0 };
-  public imagedata?:string;
-}
